29cd55bc6d6e3dfb439add7aed353cc5a180809f
Vishwajit Satish Sayambar <vishwajitsayambar@gmail.com>
2023-01-02 23:12:42 +0530

    Created Auth controller

---
+++ app/Http/Controllers/AuthCntroller.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthCntroller extends Controller
+{
+    public function register(Request $request){
+        $user = User::create([
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            'email' => $request->input('email'),
+            'used_for' => $request->input('used_for'),
+            'password' => Hash::make($request->input('password'))
+        ]);
+
+        return response([
+            'message' => 'Register Successfully!'
+        ], Response::HTTP_CREATED);
+    }
+
+    public function login(Request $request){
+        if(!Auth::attempt($request->only('email','password'))){
+            return response([
+                'message' => 'Invalid Credentials! Please try again'
+            ], Response::HTTP_UNAUTHORIZED);
+           }
+        $user = Auth::user();
+//        return $user; // this will return UserData object
+        $token = $user->createToken('token')->plainTextToken;
+              return response()->json(['token' => $token]); //this will  return token to response
+
+
+//        $cookie = Cookie('jwt' , $token, 60*24); // cookie remain For 1 day
+//        return response([
+//            'message' => 'successfully login!',
+//            $cookie
+//        ],Response::HTTP_OK)->withCookie($cookie);
+
+    }
+    public function user(){
+        return Auth::user();
+    }
+}
