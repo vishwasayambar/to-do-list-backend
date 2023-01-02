0ddad6035aabe3ce35f3d37c3c41522eda293a9b
Vishwajit Satish Sayambar <vishwajitsayambar@gmail.com>
2023-01-02 23:13:21 +0530

    Created Migration of user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('used_for')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
