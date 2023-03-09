de1794e0bc28512654b22019152433b63dfeccf2
Vishwajit Satish Sayambar <vishwajitsayambar@gmail.com>
2023-03-10 00:02:55 +0530

    Run migration for Creating workspace column and model and controller

---
+++ app/Http/Controllers/WorkspacesController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Workspace;
+use Illuminate\Http\Request;
+
+class WorkspacesController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show(Workspace $workspace)
+    {
+    }
+
+    public function edit(Workspace $workspace)
+    {
+    }
+
+    public function update(Request $request, Workspace $workspace)
+    {
+    }
+
+    public function destroy(Workspace $workspace)
+    {
+    }
+}
